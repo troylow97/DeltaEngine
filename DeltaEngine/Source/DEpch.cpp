ee243bb929754c3f41f34c6e20e3d7121df479fd
clarachin <55922842+clarachin@users.noreply.github.com>
2020-04-20 16:08:21 +0800

    pch

---
+++ DeltaEngine/Source/DEpch.cpp
@@ -0,0 +1,1 @@
+#include "DEpch.h"
---
+++ DeltaEngine/Source/DEpch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// common headers
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+
+// STL
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+// platform specifi header
+#ifdef DE_PLATFORM_WINDOWS
+#include <Windows.h>
+#endif
