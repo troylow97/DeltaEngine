e471b9f00468ddf9725d9fc73146188712623270
Spencer Tan <spencertan96@gmail.com>
2020-09-25 11:15:40 +0800

    Full Merged except Collision System

--- premake5.lua
+++ premake5.lua
@@ -27,7 +27,7 @@ project "DeltaEngine"
     "%{prj.name}/Dep/imgui/examples/imgui_impl_opengl3.cpp",
     "%{prj.name}/Dep/imgui/examples/imgui_impl_win32.h",
     "%{prj.name}/Dep/imgui/examples/imgui_impl_win32.cpp",
-    "%{prj.name}/Dep/spdlog/include/ft2build.h",
+    "%{prj.name}/Dep/freetype/include/ft2build.h",
     "%{prj.name}/Dep/stb_image/stb_image.h",
     "%{prj.name}/Dep/stb_image/stb_image.cpp",
     "%{prj.name}/Source/**.h",
@@ -55,6 +55,7 @@ project "DeltaEngine"
   {
     "glew32s.lib",
     "OpenGL32.lib",
+    "freetype.lib"
   }
 
   filter "system:windows" 
@@ -67,7 +68,8 @@ project "DeltaEngine"
       "WIN32",
       "GLEW_STATIC",
       "DE_PLATFORM_WINDOWS",
-      "DE_BUILD_DLL"
+      "DE_BUILD_DLL",
+      "FT2_BUILD_LIBRARY"
     }
   
   postbuildcommands
