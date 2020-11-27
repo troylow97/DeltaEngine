5f77f8b1d2bb6da031510877dad8ea07661db8f6
Spencer Tan <spencertan96@gmail.com>
2020-11-27 12:52:22 +0800

    Reordering Panels for ImGui Active

---
+++ DeltaEngine/Source/ImGui/Panels/StylePanel.cpp
@@ -0,0 +1,23 @@
+#include "ImGui/Panels/StylePanel.h"
+#include "ECS/ECSModule.h"
+#include "ImGui/Editor.h"
+namespace DeltaEngine
+{
+StylePanel::StylePanel( std::string str ) :
+  IPanel( str )
+{
+  m_enabled = true;
+}
+
+StylePanel::~StylePanel()
+{
+  m_enabled = false;
+}
+
+void StylePanel::Render()
+{
+  if (ImGui::Begin( m_name.c_str(), &m_enabled ))
+    ImGui::ShowStyleEditor();
+  ImGui::End();
+}
+}
