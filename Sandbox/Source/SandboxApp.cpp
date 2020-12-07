9d45134033733ed3cefa807199a86f52c6df61c2
clarachin <55922842+clarachin@users.noreply.github.com>
2020-12-07 17:33:25 +0800

    pushing for troy

--- Sandbox/Source/SandboxApp.cpp
+++ Sandbox/Source/SandboxApp.cpp
@@ -25,10 +25,10 @@ public:
     env.pECS->GetWorld().Load("World/MainMenu.json");
   	
     CollisionSystem::collision_handler.RegisterOnStay(CollisionHandlerFunctions::TakeDamage);
-    //env.pECS->GetWorld().CreateSystems<AttackSystem, EnemySpawner, LifespanSystem, RespawnSystem, GCameraSystem, UISystem, UpgradeSystem>();
-    //env.pECS->GetWorld().SetUpdateSequence<AttackSystem, EnemySpawner, LifespanSystem, RespawnSystem, UpgradeSystem>();
-    //env.pECS->GetWorld().SetLateUpdateSequence<GCameraSystem, UISystem>();
-    //env.pECS->GetWorld().InitSystems();
+    env.pECS->GetWorld().CreateSystems<AttackSystem, EnemySpawner, LifespanSystem, RespawnSystem, GCameraSystem, UISystem, UpgradeSystem>();
+    env.pECS->GetWorld().SetUpdateSequence<AttackSystem, EnemySpawner, LifespanSystem, RespawnSystem, UpgradeSystem>();
+    env.pECS->GetWorld().SetLateUpdateSequence<GCameraSystem, UISystem>();
+    env.pECS->GetWorld().InitSystems();
     CollisionHandlerFunctions::Initialise();
 
     AudioEngine::Play("Audio/MainMenu/main_menu_bgm.wav");
