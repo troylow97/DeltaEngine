25387f60fb1eb968999824ca74e022021ad8b1e6
troylow97 <troylow_97@hotmail.com>
2021-01-29 11:56:28 +0800

    Added in enemies and some colliders for level

--- DeltaEngine/Source/DeltaEngine.h
+++ DeltaEngine/Source/DeltaEngine.h
@@ -1,3 +1,16 @@
+/**********************************************************************************
+* \file   DeltaEngine.h
+* \brief  The file contains BLAHBLAHBLAH
+* \author Chin, Clara,   X% Code Contribution
+* \author Low, Troy,     X% Code Contribution
+* \author Ong, Graeme,   X% Code Contribution
+* \author Tan, Tong Wee, X% Code Contribution
+*
+*
+* \copyright Copyright (c) 2020 DigiPen Institute of Technology. Reproduction
+or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+**********************************************************************************/
 #pragma once
 //For use by Delta Engine Applications
 #include "Application.h"
@@ -28,7 +41,7 @@
 #include "Render/Mesh.h"
 #include "Render/OpenGLSystem.h"
 #include "Render/Shader.h"
-#include "Render/Texture.h"	
+#include "Render/Texture.h"
 #include "Render/ParticleSystem.h"
 #include "Render/Material.h"
 
