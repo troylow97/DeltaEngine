e872c6f8240c4d0dc8d420e7cd652d7f9ba1bdce
troylow97 <troylow_97@hotmail.com>
2021-02-04 23:35:19 +0800

    Revert "Camera crash bug still not fixed"
    This reverts commit 3a6a3d57eef63047d058bfdfd841fa14752123ba.

--- DeltaEngine/Source/Render/Camera.cpp
+++ DeltaEngine/Source/Render/Camera.cpp
@@ -18,215 +18,215 @@ written consent of DigiPen Institute of Technology is prohibited.
 
 namespace DeltaEngine
 {
-    std::vector<Camera*> Camera::allCameras;
-    FrameBuffer* Camera::finalFrameBuffer;
-    Camera* Camera::editorCamera;
-    Transform Camera::editorCameraTransform;
-    float Camera::fixedAspect = 16.f / 9.f;
+  std::vector<Camera*> Camera::allCameras;
+  FrameBuffer* Camera::finalFrameBuffer;
+  Camera* Camera::editorCamera;
+  Transform Camera::editorCameraTransform;
+  float Camera::fixedAspect = 16.f / 9.f;
 
-    Camera::Camera(bool editor) :
-        cameraIndex{ editor ? -1 : static_cast<int>(allCameras.size()) },
-        frameBuffer{},
-        aspectRatio{ 16.f / 9.f },
-        viewportSize{ 1920 },
-        camWidth{ 1920 },
-        m_Size{ 6 },
-        m_zNear{ -100 },
-        m_zFar{ 100 },
-        backgroundColor{ 49 / 255.0f, 77 / 255.0f, 121 / 255.0f, 1 }
+  Camera::Camera(bool editor) :
+    cameraIndex{editor ? -1 : static_cast<int>(allCameras.size())},
+    frameBuffer{},
+    aspectRatio{ 16.f / 9.f },
+    viewportSize{ 1920 },
+    camWidth{ 1920 },
+    m_Size{6},
+    m_zNear{-100},
+    m_zFar{100},
+    backgroundColor{49 / 255.0f, 77 / 255.0f, 121 / 255.0f, 1}
+  {
+    if (!editor)
+      allCameras.push_back(this);
+    else
     {
-        if (!editor)
-            allCameras.push_back(this);
-        else
-        {
-            if (!editorCamera)
-            {
-                editorCamera = this;
-                editorCameraTransform = Transform();
-            }
-            else
-            {
-                DeltaEngine_CORE_ERROR("An editor camera already exists, only one should exist at a time.");
-                delete this;
-            }
-        }
+      if (!editorCamera)
+      {
+        editorCamera = this;
+        editorCameraTransform = Transform();
+      }
+      else
+      {
+        DeltaEngine_CORE_ERROR("An editor camera already exists, only one should exist at a time.");
+        delete this;
+      }
     }
-    Camera::Camera(const Camera& copy) :
-        cameraIndex{ static_cast<int>(allCameras.size()) },
-        frameBuffer{},
-        aspectRatio{ 16.f / 9.f },
-        viewportSize{ 1920 },
-        camWidth{ 1920 },
-        m_Size{ copy.m_Size },
-        m_zNear{ copy.m_zNear },
-        m_zFar{ copy.m_zFar },
-        backgroundColor{ copy.backgroundColor }
+  }
+  Camera::Camera(const Camera& copy) :
+    cameraIndex{ static_cast<int>(allCameras.size()) },
+    frameBuffer{},
+    aspectRatio{ 16.f / 9.f },
+    viewportSize{ 1920 },
+    camWidth{ 1920 },
+    m_Size{ copy.m_Size },
+    m_zNear{ copy.m_zNear },
+    m_zFar{ copy.m_zFar },
+    backgroundColor{ copy.backgroundColor }
+  {
+    if (&copy == editorCamera)
     {
-        if (&copy == editorCamera)
-        {
-            DeltaEngine_CORE_ERROR("Copying the editor camera is not allowed!");
-            delete this;
-        }
-        else
-            allCameras.push_back(this);
+      DeltaEngine_CORE_ERROR("Copying the editor camera is not allowed!");
+      delete this;
     }
+    else
+      allCameras.push_back(this);
+  }
 
-    Camera::Camera(Camera&& move) :
-        cameraIndex{ static_cast<int>(allCameras.size()) },
-        frameBuffer{},
-        aspectRatio{ 16.f / 9.f },
-        viewportSize{ 1920 },
-        camWidth{ 1920 },
-        m_Size{ move.m_Size },
-        m_zNear{ move.m_zNear },
-        m_zFar{ move.m_zFar },
-        backgroundColor{ move.backgroundColor }
+  Camera::Camera(Camera&& move) :
+    cameraIndex{ static_cast<int>(allCameras.size()) },
+    frameBuffer{},
+    aspectRatio{ 16.f / 9.f },
+    viewportSize{ 1920 },
+    camWidth{ 1920 },
+    m_Size{ move.m_Size },
+    m_zNear{ move.m_zNear },
+    m_zFar{ move.m_zFar },
+    backgroundColor{ move.backgroundColor }
+  {
+    if (&move == editorCamera)
     {
-        if (&move == editorCamera)
-        {
-            DeltaEngine_CORE_ERROR("Copying the editor camera is not allowed!");
-            delete this;
-        }
-        else
-            allCameras.push_back(this);
+      DeltaEngine_CORE_ERROR("Copying the editor camera is not allowed!");
+      delete this;
     }
+    else
+      allCameras.push_back(this);
+  }
 
-    Camera::~Camera()
+  Camera::~Camera()
+  {
+    if (cameraIndex >= 0)
     {
-        if (cameraIndex >= 0)
-        {
-            allCameras.erase(allCameras.begin() + cameraIndex);
+      allCameras.erase(allCameras.begin() + cameraIndex);
 
-            for (int i = 0; i < allCameras.size(); ++i)
-                allCameras[i]->cameraIndex = i;
-        }
+      for (int i = 0; i < allCameras.size(); ++i)
+        allCameras[i]->cameraIndex = i;
     }
+  }
 
-    Camera& Camera::operator=(const Camera& copy)
+  Camera& Camera::operator=(const Camera& copy)
+  {
+    if (&copy == editorCamera)
     {
-        if (&copy == editorCamera)
-        {
-            DeltaEngine_CORE_ERROR("Copying the editor camera is not allowed!");
-            return *this;
-        }
-        m_Size = copy.m_Size;
-        m_zNear = copy.m_zNear;
-        m_zFar = copy.m_zFar;
-        backgroundColor = copy.backgroundColor;
-        return *this;
+      DeltaEngine_CORE_ERROR("Copying the editor camera is not allowed!");
+      return *this;
     }
+    m_Size = copy.m_Size;
+    m_zNear = copy.m_zNear;
+    m_zFar = copy.m_zFar;
+    backgroundColor = copy.backgroundColor;
+    return *this;
+  }
 
-    Camera& Camera::operator=(Camera&& move)
+  Camera& Camera::operator=(Camera&& move)
+  {
+    // moving camera should not be allowed, just copy
+    if (&move == editorCamera)
     {
-        // moving camera should not be allowed, just copy
-        if (&move == editorCamera)
-        {
-            DeltaEngine_CORE_ERROR("Copying the editor camera is not allowed!");
-            return *this;
-        }
-        m_Size = move.m_Size;
-        m_zNear = move.m_zNear;
-        m_zFar = move.m_zFar;
-        backgroundColor = move.backgroundColor;
-        return *this;
+      DeltaEngine_CORE_ERROR("Copying the editor camera is not allowed!");
+      return *this;
     }
+    m_Size = move.m_Size;
+    m_zNear = move.m_zNear;
+    m_zFar = move.m_zFar;
+    backgroundColor = move.backgroundColor;
+    return *this;
+  }
 
-    Matrix4x4 Camera::GetProjectionMatrix(Transform transform) const
-    {
-        if (this == editorCamera)
-            return Matrix4x4::Rotate(transform.rotation) * Matrix4x4::Ortho(
-                -m_Size * aspectRatio,
-                m_Size * aspectRatio,
-                -m_Size, m_Size, m_zNear, m_zFar);
-        else
-            return Matrix4x4::Rotate(transform.rotation) * Matrix4x4::Ortho(
-                -m_Size * ((fixedAspect > .01f) ? fixedAspect : aspectRatio),
-                m_Size * ((fixedAspect > .01f) ? fixedAspect : aspectRatio),
-                -m_Size, m_Size, m_zNear, m_zFar);
-    }
+  Matrix4x4 Camera::GetProjectionMatrix(Transform transform) const
+  {
+    if (this == editorCamera)
+      return Matrix4x4::Rotate(transform.rotation) * Matrix4x4::Ortho(
+        -m_Size * aspectRatio,
+        m_Size * aspectRatio,
+        -m_Size, m_Size, m_zNear, m_zFar);
+    else
+      return Matrix4x4::Rotate(transform.rotation) * Matrix4x4::Ortho(
+        -m_Size * ((fixedAspect > .01f ) ? fixedAspect : aspectRatio),
+        m_Size * ((fixedAspect > .01f ) ? fixedAspect : aspectRatio),
+        -m_Size, m_Size, m_zNear, m_zFar);
+  }
 
-    Matrix4x4 Camera::GetViewMatrix(Transform transform) const
-    {
-        return Matrix4x4::Transpose(Matrix4x4::Translate(-transform.position));
-    }
+  Matrix4x4 Camera::GetViewMatrix(Transform transform) const
+  {
+    return Matrix4x4::Transpose(Matrix4x4::Translate(-transform.position));
+  }
 
-    Vector3 Camera::Max(Transform transform) const
-    {
-        return Vector3(m_Size * aspectRatio + transform.position.x, m_Size + transform.position.y);
-    }
+  Vector3 Camera::Max(Transform transform) const
+  {
+    return Vector3(m_Size * aspectRatio + transform.position.x, m_Size + transform.position.y);
+  }
 
-    Vector3 Camera::Min(Transform transform) const
-    {
-        return Vector3(-m_Size * aspectRatio + transform.position.x, -m_Size + transform.position.y);
-    }
+  Vector3 Camera::Min(Transform transform) const
+  {
+    return Vector3(-m_Size * aspectRatio + transform.position.x, -m_Size + transform.position.y);
+  }
 
-    FrameBuffer& Camera::GetFrameBuffer()
-    {
-        return frameBuffer;
-    }
+  FrameBuffer& Camera::GetFrameBuffer()
+  {
+    return frameBuffer;
+  }
 
-    float Camera::GetAspectRatio()
-    {
-        return aspectRatio;
-    }
+  float Camera::GetAspectRatio()
+  {
+    return aspectRatio;
+  }
+  
+  float Camera::SetAspectRatio(float width, float height)
+  {
+    aspectRatio = width / height;
+    return aspectRatio;
+  }
 
-    float Camera::SetAspectRatio(float width, float height)
-    {
-        aspectRatio = width / height;
-        return aspectRatio;
-    }
+  float Camera::GetFixedAspectRatio()
+  {
+    return fixedAspect;
+  }
 
-    float Camera::GetFixedAspectRatio()
-    {
-        return fixedAspect;
-    }
+  float Camera::SetFixedAspectRatio(float width, float height)
+  {
+    fixedAspect = width / height;
+    return fixedAspect;
+  }
 
-    float Camera::SetFixedAspectRatio(float width, float height)
-    {
-        fixedAspect = width / height;
-        return fixedAspect;
-    }
+  float Camera::GetViewportSize()
+  {
+    return camWidth;
+  }
 
-    float Camera::GetViewportSize()
-    {
-        return camWidth;
-    }
+  float Camera::SetViewportSize(float width)
+  {
+    viewportSize = width;
+    camWidth = viewportSize;
+    if (fixedAspect > .01f && aspectRatio > fixedAspect)
+      camWidth *= fixedAspect / aspectRatio;
+    return camWidth;
+  }
 
-    float Camera::SetViewportSize(float width)
-    {
-        viewportSize = width;
-        camWidth = viewportSize;
-        if (fixedAspect > .01f && aspectRatio > fixedAspect)
-            camWidth *= fixedAspect / aspectRatio;
-        return camWidth;
-    }
+  float Camera::GetTrueViewportSize()
+  {
+    return viewportSize;
+  }
 
-    float Camera::GetTrueViewportSize()
-    {
-        return viewportSize;
-    }
+  void Camera::Start()
+  {
+    frameBuffer.Resize(static_cast<unsigned int>(camWidth),
+                       static_cast<unsigned int>(camWidth / fixedAspect));
+    frameBuffer.Bind();
 
-    void Camera::Start()
-    {
-        frameBuffer.Resize(static_cast<unsigned int>(camWidth),
-            static_cast<unsigned int>(camWidth / fixedAspect));
-        frameBuffer.Bind();
+    //glEnable(GL_DEPTH_TEST);
+    glClearColor(
+      backgroundColor.r,
+      backgroundColor.g,
+      backgroundColor.b,
+      backgroundColor.a);
+    glClear(GL_COLOR_BUFFER_BIT /*| GL_DEPTH_BUFFER_BIT*/);
+  }
 
-        //glEnable(GL_DEPTH_TEST);
-        glClearColor(
-            backgroundColor.r,
-            backgroundColor.g,
-            backgroundColor.b,
-            backgroundColor.a);
-        glClear(GL_COLOR_BUFFER_BIT /*| GL_DEPTH_BUFFER_BIT*/);
-    }
-
-    void Camera::End()
-    {
-        if (this == editorCamera)
-            Gizmos::DrawWorldGrid();
-        frameBuffer.Unbind();
-        //glDisable(GL_DEPTH_TEST);
-        glClear(GL_COLOR_BUFFER_BIT);
-    }
+  void Camera::End()
+  {
+    if (this == editorCamera)
+      Gizmos::DrawWorldGrid();
+    frameBuffer.Unbind();
+    //glDisable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT);
+  }
 }
