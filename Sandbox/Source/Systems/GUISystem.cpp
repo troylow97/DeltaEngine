14bf3536116480e35e661cebd948a03192eeeba5
Spencer <spencertan96@gmail.com>
2021-02-10 21:50:51 +0800

    Fixing GUI Checking*

--- Sandbox/Source/Systems/GUISystem.cpp
+++ Sandbox/Source/Systems/GUISystem.cpp
@@ -46,7 +46,7 @@ void GUISystem::LateUpdate()
   auto p_x = InputManager::Instance().CurrentPosition().point_x - GamePanel::render_pos.x;
   auto p_y = InputManager::Instance().CurrentPosition().point_y - GamePanel::render_pos.y;
 #else
-  auto p_x= InputManager::Instance().CurrentPosition().point_x - GetEnv().pWin->ClientTopLeft().point_x;
+  auto p_x = InputManager::Instance().CurrentPosition().point_x - GetEnv().pWin->ClientTopLeft().point_x;
   auto p_y = InputManager::Instance().CurrentPosition().point_y - GetEnv().pWin->ClientTopLeft().point_y;
 #endif
   auto t_aspect = GamePanel::render_size.x / GamePanel::render_size.y;
@@ -59,30 +59,30 @@ void GUISystem::LateUpdate()
   em.ForEach( [&]( RendererOverlay &r, Image &i )
   {
     auto coords = r.GetScreenspaceBounds( i );
-    float zeroPos = (1 - c.GetFixedAspectRatio() / t_aspect) / 2;
+    float zeroPos = ( 1 - c.GetFixedAspectRatio() / t_aspect ) / 2;
     if ( c.GetFixedAspectRatio() > c.GetAspectRatio() )
     {
-      float halfRatioY = coords.y < 0.5f ? coords.y * 2 : (coords.y - 0.5f) * 2;
-      float halfRatioW = coords.w < 0.5f ? coords.w * 2 : (coords.w - 0.5f) * 2;
+      float halfRatioY = coords.y < 0.5f ? coords.y * 2 : ( coords.y - 0.5f ) * 2;
+      float halfRatioW = coords.w < 0.5f ? coords.w * 2 : ( coords.w - 0.5f ) * 2;
 
       std::cerr << coords.y << ", " << coords.w << std::endl;
       std::cerr << c.GetFixedAspectRatio() << ", " << t_aspect << std::endl;
 
-      coords.y = coords.y < 0.5f ? zeroPos + (0.5f - zeroPos) * halfRatioY : 1 - zeroPos - (0.5f - zeroPos) * halfRatioY;
-      coords.w = coords.w < 0.5f ? zeroPos + (0.5f - zeroPos) * halfRatioW : 1 - zeroPos - (0.5f - zeroPos) * halfRatioW;
+      coords.y = coords.y < 0.5f ? zeroPos + ( 0.5f - zeroPos ) * halfRatioY : 1 - zeroPos - ( 0.5f - zeroPos ) * halfRatioY;
+      coords.w = coords.w < 0.5f ? zeroPos + ( 0.5f - zeroPos ) * halfRatioW : 1 - zeroPos - ( 0.5f - zeroPos ) * halfRatioW;
 
       std::cerr << coords.y << ", " << coords.w << std::endl;
     }
     else
     {
-      float halfRatioX = coords.x < 0.5f ? coords.x * 2 : (1 - coords.x) * 2;
-      float halfRatioZ = coords.z < 0.5f ? coords.z * 2 : (1 - coords.z) * 2;
+      float halfRatioX = coords.x < 0.5f ? coords.x * 2 : ( 1 - coords.x ) * 2;
+      float halfRatioZ = coords.z < 0.5f ? coords.z * 2 : ( 1 - coords.z ) * 2;
 
       std::cerr << coords.x << ", " << coords.z << std::endl;
       std::cerr << c.GetFixedAspectRatio() << ", " << t_aspect << std::endl;
 
-      coords.x = coords.x < 0.5f ? zeroPos + (0.5f - zeroPos) * halfRatioX : 1 - zeroPos - (0.5f - zeroPos) * halfRatioX;
-      coords.z = coords.z < 0.5f ? zeroPos + (0.5f - zeroPos) * halfRatioZ : 1 - zeroPos - (0.5f - zeroPos) * halfRatioZ;
+      coords.x = coords.x < 0.5f ? zeroPos + ( 0.5f - zeroPos ) * halfRatioX : 1 - zeroPos - ( 0.5f - zeroPos ) * halfRatioX;
+      coords.z = coords.z < 0.5f ? zeroPos + ( 0.5f - zeroPos ) * halfRatioZ : 1 - zeroPos - ( 0.5f - zeroPos ) * halfRatioZ;
 
       std::cerr << coords.x << ", " << coords.z << std::endl;
     }
@@ -91,11 +91,12 @@ void GUISystem::LateUpdate()
     coords.z *= width;
     coords.w *= height;
 
+    if ( CollisionIntersection_RectMinMaxMouse( { coords.x, coords.y }, { coords.z, coords.w }, { p_x, p_y } ) )
     {
-      DeltaEngine_CORE_INFO("Coords Min: {},{}    Max: {},{}", coords.x, coords.y, coords.z, coords.w);
-      DeltaEngine_CORE_INFO("Mouse: {},{}", p_x, p_y);
+      DeltaEngine_CORE_INFO( "Coords Min: {},{}    Max: {},{}", coords.x, coords.y, coords.z, coords.w );
+      DeltaEngine_CORE_INFO( "Mouse: {},{}", p_x, p_y );
 
-      DeltaEngine_CORE_INFO("width: {}, height: {}", width, height);
+      DeltaEngine_CORE_INFO( "width: {}, height: {}", width, height );
     }
   } );
 
